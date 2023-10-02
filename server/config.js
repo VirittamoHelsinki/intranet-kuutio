c2defc19803506b49dfce6b4146767b96227df8a
Veikko Laine <veikko.laine@gmail.com>
2023-10-02 10:07:57 +0300

    Updates

--- server/config.js
+++ server/config.js
@@ -1,12 +1,19 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+const environment = process.env.NODE_ENV
 const PORT = process.env.PORT
-const mongoUri = process.env.MONGODB_URI
 
-const users_url = process.env.USERS_URL
+let mongoUri = process.env.MONGODB_URI_DEV
+let users_url = process.env.USERS_URL_DEV
+let service_name = process.env.SERVICE_NAME_DEV
+let service_key = process.env.SERVICE_KEY_DEV
 
-const service_name = process.env.SERVICE_NAME
-const service_key = process.env.SERVICE_KEY
+if (environment === 'production'){
+    mongoUri = process.env.MONGODB_URI_PROD
+    users_url = process.env.USERS_URL_PROD
+    service_name = process.env.SERVICE_NAME_PROD
+    service_key = process.env.SERVICE_KEY_PROD
+}
 
-export { PORT, mongoUri, users_url, service_name, service_key }
+export { environment, PORT, mongoUri, users_url, service_name, service_key }
