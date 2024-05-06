cb24b134b2d6cc3929b851143ac85e6108236f23
Roope Vuorenlehto <roope.vuorenlehto@edu.hel.fi>
2024-05-06 15:00:32 +0300

    Use selectedBookings in BookingListPage. Show only bookings for selectedDate

--- client/src/components/BookingListPage.jsx
+++ client/src/components/BookingListPage.jsx
@@ -2,46 +2,47 @@ import React, { useState, useEffect } from "react";
 import bookingApi from "../api/booking";
 import "../styles/BookingPage.scss";
 
-const BookingListPage = ({ bookings, onBookingsUpdate }) => {
+export const sortBookings = (filteredBookings) => {
+	const sortedBookings = filteredBookings.sort((a, b) => {
+		const dateComparison = new Date(a.selectedDate).getTime() - new Date(b.selectedDate).getTime();
+		if (dateComparison !== 0) {
+			return dateComparison;
+		}
+		else {
+			const timeA = a.selectedTime[0];
+			const timeB = b.selectedTime[0];
+			return timeA.localeCompare(timeB);
+		}
+	});
+	return sortedBookings;
+};
 
-	const sortBookings = (filteredBookings) => {
-		const sortedBookings = filteredBookings.sort((a, b) => {
-			const dateComparison = new Date(a.selectedDate).getTime() - new Date(b.selectedDate).getTime();
-			if (dateComparison !== 0) {
-				return dateComparison;
-			}
-			else {
-				const timeA = a.selectedTime[0];
-				const timeB = b.selectedTime[0];
-				return timeA.localeCompare(timeB);
-			}
-		});
-		return sortedBookings;
-	};
+const BookingListPage = ({ onBookingsUpdate , selectedDate, selectedBookings}) => {
 
   useEffect(() => {
     // Fetch bookings from the backend when the component mounts
     bookingApi
       .getAll()
       .then((response) => {
-        // Filter out bookings that are older than the current date
-        const currentDate = new Date();
+		  // Filter out bookings that are older than the current date
+		  const currentDate = new Date();
 
-        const filteredBookings = response.data.data.filter((booking) => {
-          const bookingDate = new Date(booking.selectedDate);
+		  const filteredBookings = response.data.data.filter((booking) => {
+			  const bookingDate = new Date(booking.selectedDate);
 
-		  // +1 error somewhere in dates, didnt show currentday bookings without this
-		  bookingDate.setDate(bookingDate.getDate() + 1);
+			  // +1 error somewhere in dates, didnt show currentday bookings without this
+			  bookingDate.setDate(bookingDate.getDate() + 1);
 
-          return bookingDate >= currentDate;
-        });
+			  return bookingDate >= currentDate;
+			});
 
-        onBookingsUpdate(sortBookings(filteredBookings));
-      })
-      .catch((error) => {
-        console.error("Error fetching bookings:", error);
-      });
-  }, []);
+			onBookingsUpdate(sortBookings(filteredBookings));
+		})
+		.catch((error) => {
+			console.error("Error fetching bookings:", error);
+		});
+	}, [selectedDate]);
+
 
   const handleRemoveBooking = (id) => {
     // Send a request to your API to remove the booking with the given ID
@@ -60,7 +61,7 @@ const BookingListPage = ({ bookings, onBookingsUpdate }) => {
     <div>
       <h2>Varauslista</h2>
       <div className="booking-cards">
-        {bookings.map((booking) => (
+         {selectedBookings.map((booking) => (
           <div key={booking._id} className="booking-card">
             <div className="card-content">
               <strong>Aihe: </strong> {booking.topic}
